ff06a4eecaa667e7edfd6b0bbf20d57e8117fa6f
Inga Vishnivetskaia <inga_vishnivetskaia@epam.com>
2022-10-10 01:36:34 +0300

    add products list

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
 import Header from "~/components/MainLayout/components/Header";
 import Box from "@mui/material/Box";
+import CardContent from "@mui/material/CardContent";
 
 function Copyright() {
   return (
@@ -18,13 +21,39 @@ function Copyright() {
   );
 }
 
-const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const MainLayout: React.FC<{ children: React.ReactNode }> = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://g3llsjnhed.execute-api.us-east-1.amazonaws.com/dev/products"
+      )
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []);
   return (
     <>
       <Header />
       <main>
         <Container sx={{ pb: 8 }} maxWidth="md">
-          {children}
+          {products.map(
+            (product: {
+              productId: string;
+              productName: string;
+              price: number;
+            }) => {
+              return (
+                <Card sx={{ minWidth: 275 }} key={product.productId}>
+                  <CardContent>
+                    <Typography variant="h6">{product.productName}</Typography>
+                    <p className="post-body">Price: {product.price} $</p>
+                  </CardContent>
+                </Card>
+              );
+            }
+          )}
         </Container>
       </main>
       <Box
